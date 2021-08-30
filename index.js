aeb6bb5a47c32bfc6fedddfa5764d649813f6f15
Steven Brock <shyfay0@gmail.com>
2021-08-30 14:25:46 -0700

    commit tree ab11f841f1cfde006f897782143dff512e3c692b

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  else if (distance > 2500){
+    return "No can do.";
+  }
+  
+  else if(distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  
+
+}
   // Write your code here!
+
+
+function ternaryCheckCity(city){
+if (city === "NYC"){
+  return "Ok, sounds good.";
+}
+    else { 
+      return "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+switch (tip){
+  case "generous" : 
+  return "Thank you so much.";
+break;
+
+  case "not as generous" : 
+  return "Thank you.";
+break;
+
+  default:
+    return "Bye."
+}}
